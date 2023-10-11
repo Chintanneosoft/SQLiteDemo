475b112599425ae0066178051a5bf2334885cae0
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-10-11 12:01:39 +0530

    SQLiteDemo

---
+++ DBDemo/DBDemo/Person.swift
@@ -0,0 +1,17 @@
+
+import Foundation
+
+class Person
+{
+    var id: Int
+    var name: String = ""
+    var exp: String = ""
+    
+    init(id: Int,name:String, exp:String)
+    {
+        self.id = id
+        self.name = name
+        self.exp = exp
+    }
+
+}
